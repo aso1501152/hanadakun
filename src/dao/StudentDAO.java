df90955eedded8b82fe24af15bcf0c2bcf17c092
rrr <1501164@st.asojuku.ac.jp>
2017-06-06 14:21:57 +0900

    dao

---
+++ src/dao/StudentDAO.java
@@ -0,0 +1,98 @@
+/*
+ * date:2017/06/06
+ * name:福永利恵
+ * comm:studentテーブルのDAO
+ *
+ *
+ * とりあえず基本的なsqlをコメントアウトして入れてます適宜変更追加よろしく
+ *
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class StudentDAO {
+	// データソース
+	DataSource ds = null;
+	// データベース接続情報
+	Connection con = null;
+	// プリコンパイル用のステートメント
+	PreparedStatement stmt = null;
+	// SELECTの結果を格納するResultSet
+	ResultSet rs = null;
+
+	/**
+	 * データベースへの接続処理を行うメソッド
+	 *
+	 * @return コネクション情報
+	 */
+	public Connection connection() throws Exception {
+		// データソースがなければ、context.xmlから読み込んで設定する
+		if (ds == null) {
+			ds = (DataSource) (new InitialContext()).lookup("java:comp/env/jdbc/MySQL");
+		}
+		con = ds.getConnection();
+		return con;
+	}
+
+	/**
+	 * データベースからの切断処理を行うメソッド
+	 */
+	public void close() throws Exception {
+		// データベース接続されていれば、切断する
+		if (rs != null) {
+			rs.close();
+		}
+		if (stmt != null) {
+			stmt.close();
+		}
+		if (con != null) {
+			con.close();
+		}
+	}
+
+	/**
+	 * userテーブルからログインするユーザを探す
+
+	public User selectLoginUser(String userID, String password) {
+		// ログインユーザ情報を格納
+		User us = new User();
+		try {
+			// DB接続
+			connection();
+			// SQL文設定の準備・SQL文の実行
+			String sql = "SELECT * FROM user WHERE userid = ? AND password = ?;";
+
+			stmt = con.prepareStatement(sql);// sql文をプリコンパイルした状態で保持
+			// ユーザの入力値を代入
+			stmt.setString(1, userID);
+			stmt.setString(2, password);
+
+			// sql文を実行
+			rs = stmt.executeQuery();
+
+			// 1件目のデータにカーソルをあわせる
+			// データない場合はcatchに飛ぶ
+			rs.next();
+
+			// DBから取得したデータをuserオブジェクトに格納
+			us.setUserid(rs.getString("userid"));
+			us.setPassword(rs.getString("password"));
+			us.setGrant(rs.getString("grant"));
+		} catch (Exception e) {
+			us = null;
+			System.out.println("muri");
+		} finally {
+			try {
+				close();
+			} catch (Exception e) {
+		}
+	}
+	 */
+}
+
