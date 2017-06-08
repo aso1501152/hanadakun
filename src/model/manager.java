39c2e1b2133d27a4a3f1b6ba8809933e7c3e8b6c
aso1501133 <1501133@st.asojuku.ac.jp>
2017-06-08 14:08:27 +0900

    ooooo

---
+++ src/model/manager.java
@@ -0,0 +1,32 @@
+package model;
+
+public class manager {
+	private String teacher_id;
+	private String teacher_name;
+	private String password;
+	private String class_id;
+	public String getTeacher_id() {
+		return teacher_id;
+	}
+	public void setTeacher_id(String teacher_id) {
+		this.teacher_id = teacher_id;
+	}
+	public String getTeacher_name() {
+		return teacher_name;
+	}
+	public void setTeacher_name(String teacher_name) {
+		this.teacher_name = teacher_name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getClass_id() {
+		return class_id;
+	}
+	public void setClass_id(String class_id) {
+		this.class_id = class_id;
+	}
+}
